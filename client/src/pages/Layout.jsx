f67c6552ade625381281b599f2c8d99c041a41ad
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-08-17 22:04:58 +0530

    feat: integrate user authentication with SignIn component in Layout

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -4,11 +4,13 @@ import { assets } from "../assets/assets";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import Sidebar from "../components/Sidebar";
+import { SignIn, useUser } from "@clerk/clerk-react";
 
 const Layout = () => {
   const navigate = useNavigate();
   const [sidebar, setSidebar] = useState(false);
-  return (
+  const { user } = useUser();
+  return user ? (
     <div className="flex flex-col items-start justify-start h-screen">
       <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-300">
         <img src={assets.logo} alt="Logo" onClick={() => navigate("/")} />
@@ -24,6 +26,10 @@ const Layout = () => {
           <Outlet />
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
