0ff6498a4dc8fa421ffa4dc94aeb92fbcebea9a2
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-08-17 16:31:37 +0530

    feat: add AiTools component and integrate it into Home page

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Hero = () => {
     const navigate = useNavigate();
@@ -21,6 +22,9 @@ const Hero = () => {
           Start creating now
         </button>
         <button className="bg-white px-10 py-3 rounded-lg border border-gray-400 hover:scale-102 active:scale-95 transition cursor-pointer">Watch demo</button>
+      </div>
+      <div className="flex items-center gap-4 mt-8 mx-auto text-gray-600">
+        <img src={assets.user_group} alt="" className="h-8" /> Trusted by over 100,000 users
       </div>
     </div>
   );
